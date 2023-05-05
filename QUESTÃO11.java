3f728e14e98117a40e23f162a34d272705d022b1
Pedro Braga <110943497+PedroBraga0@users.noreply.github.com>
2023-05-04 21:16:42 -0300

    Add files via upload

---
+++ QUESTÃO11.java
@@ -0,0 +1,32 @@
+
+package questão.pkg11;
+
+import java.util.Scanner;
+
+
+public class QUESTÃO11 {
+
+   
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        
+        int IDADE;
+        int ANOS;
+        int MESES;
+        
+        System.out.println("Verifique abaixo a sua idade expressa em anos, meses e dias ");
+        
+        System.out.println("IDADE");
+        IDADE = scanner.nextInt();
+        
+        System.out.println("ANOS");
+        ANOS = scanner.nextInt();
+        
+        System.out.println("MESES");
+        MESES = scanner.nextInt();
+        
+        
+    }
+    
+}
